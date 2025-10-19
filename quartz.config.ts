5821f89c508645f6bfae0c23ef01dee61cce8fff
MOHAMAD ALI ALCHIHABI <mohamadali.business@gmail.com>
2025-10-20 01:47:02 +0300

    Quartz sync: Oct 20, 2025, 1:47 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "malishehahbi.github.com/quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
